0b6cb8e7ee80370948ece33145eb4007b2cc510c
Rodrigo Cabrera <rodrigod33d@gmail.com>
2023-11-11 17:58:46 -0300

    ADD: Get serachParams en api

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 
 export function GET(req, { params }) {
-  console.log(params);
+  const { searchParams } = new URL(req.url);
+  const value = searchParams.get("name");
+  console.log(value);
   return NextResponse.json("Hello world!");
 }
